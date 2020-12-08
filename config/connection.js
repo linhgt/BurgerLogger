868ec12e7b6653522409bda154aeedc954ec6b5b
Linh Nguyen <linhnhatmen@gmail.com>
2020-12-08 10:37:29 +0700

    connect to the database

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+/*Connect to the database*/
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host:"localhost",
+    port: 3306,
+    database:"burger_db",
+    user:"root",
+    password:"Hoanhtrang@123"
+});
+
+connection.connect(function(err){
+    if (err){
+        console.log("error connecting " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
