59ccd7785b3e63d36f24b6563eda2c1ec4a5d413
Linh Nguyen <linhnhatmen@gmail.com>
2020-12-08 10:20:39 +0700

    Initial seed for the database

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+/*Initial seeds for the table*/
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Big Whoopers", FALSE);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Big Mac", FALSE);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Chicken little", FALSE);
+
