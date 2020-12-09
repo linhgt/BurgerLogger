d61a7378488f9f19aff50d46c4dee208ab2045a8
Linh Nguyen <linhnhatmen@gmail.com>
2020-12-09 15:53:17 +0700

    setting up server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+var eshbs = require("express-handlebars");
+
+var app = express();
+
+var PORT =  process.env.PORT || 4040;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Use handlebars
+app.engine("engine", eshbs({defaultLayout:"main"}));
+app.set("view engine, handlebars");
+
+//controller Route
+var controllerRoute = require("./controllers/burgers_controller");
+
+app.use(controllerRoute);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
+
