6695c088029c8c4cef98ad7066ed27b7d8ae30d2
Linh Nguyen <linhnhatmen@gmail.com>
2020-12-14 21:25:45 +0700

    small bug fix

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,1 +1,19 @@
+//Handle the controller, implementing the routes
 var express = require("express");
+
+//Create modular, mountable express router that can be exported
+var route = express.Router();
+
+var burger = require("../models/burger.js");
+
+route.get("/", function(req, res){
+    burger.selectAll(function(data){
+        var obj = {
+            burgers: data
+        };
+        console.log(obj);
+        res.render("index", obj);
+    });
+});
+
+module.exports = route;
