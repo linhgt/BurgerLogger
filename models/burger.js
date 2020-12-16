2e256dd9dd03431787e27795659381a8037a81a7
Linh Nguyen <linhnhatmen@gmail.com>
2020-12-16 23:57:29 +0700

    add comments

--- models/burger.js
+++ models/burger.js
@@ -2,16 +2,19 @@
 var orm = require("../config/orm.js");
 
 var burger = {
+    //Query retrieving all burgers
     selectAll: function(cb){
         orm.selectAll("burger", function(res){
             cb(res);
         });
     },
+    //Query inserting burger
     insertOne: function(name, devoured, cb){
         orm.insertOne("burger", name, devoured, function(res){
             cb(res);
         });
     },
+    //Query updating burger
     updateOne: function(name, devoured, id, cb){
         orm.updateOne("burger", name, devoured, id, function(res){
             cb(res);
