85ff425caddccec826b02c5086a6fd6a22f35bcd
Linh Nguyen <linhnhatmen@gmail.com>
2020-12-17 10:18:47 +0700

    fix create and update api routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,6 +6,7 @@ var route = express.Router();
 
 var burger = require("../models/burger.js");
 
+//Route for retrieving all the burger
 route.get("/", function(req, res){
     burger.selectAll(function(data){
         var obj = {
@@ -15,5 +16,22 @@ route.get("/", function(req, res){
         res.render("index", obj);
     });
 });
+
+//Route for creating a new burger
+route.post("/api/create", (req, res)=>{
+    burger.insertOne(req.body.burger_name, req.body.devoured, (result) => {
+        var obj = {
+            id: result.insertId
+        };
+        res.json(obj);
+    });
+});
+
+//Route for updating a burger
+route.put("/api/update/", (req, res) => {
+    burger.updateOne(req.body.devoured, req.body.id, (result) =>{
+        res.json({id:result.insertId});
+    });
+})
 
 module.exports = route;
