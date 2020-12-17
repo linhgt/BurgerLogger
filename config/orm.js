38c813692f436381845550e6ab77be642e95c4a9
Linh Nguyen <linhnhatmen@gmail.com>
2020-12-17 09:12:02 +0700

    add comments

--- config/orm.js
+++ config/orm.js
@@ -12,6 +12,7 @@ var orm = {
             cb(res);
         });
     },
+    //Insert a new burger to the table
     insertOne: function(tableInput, burgerName, devoured, cb){
         var Query = "INSERT INTO ?? (burger_name, devoured) VALUES (?, ?);";
         connection.query(Query, [tableInput, burgerName, devoured], (err, res)=>{
@@ -19,7 +20,8 @@ var orm = {
             cb(res);
         });
     },
-    updateOne: function(tableInput, burgerName, devoured, id, cb){
+    //Take a burger id and change it devoured state
+    updateOne: function(tableInput, devoured, id, cb){
         var Query = "UPDATE ?? SET burger_name = ?, devoured = ? WHERE id = ?";
         connection.query(Query, [tableInput, burgerName, devoured, id], (err, res)=>{
             if (err) throw err;
