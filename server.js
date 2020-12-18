a2e9e7bb7ad7bd6312703347a9c45676842f2cef
Linh Nguyen <linhnhatmen@gmail.com>
2020-12-18 09:26:12 +0700

    update 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var eshbs = require("express-handlebars");
 
 var app = express();
 
-var PORT =  process.env.PORT || 8080;
+var PORT =  process.env.PORT;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
