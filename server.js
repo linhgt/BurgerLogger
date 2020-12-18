0e8a0327826b4658dd1a609de72c1f875f1db63b
Linh Nguyen <linhnhatmen@gmail.com>
2020-12-18 09:37:20 +0700

    another fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var eshbs = require("express-handlebars");
 
 var app = express();
 
-var PORT =  process.env.PORT;
+var PORT =  process.env.PORT || 4040;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
